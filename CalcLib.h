1ddb59aabc42b1e48954349a9c06510e43088143
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2021-06-04 12:40:09 +0100

    Create CalcLib.h

---
+++ CalcLib.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace constants {
+
+	const double PI{ 3.1415926535897932384626433832795028841971693993751 };
+
+
+}
+
+//CALC OPERATIONS BY OPERAND
+
+void MainFour(int choice);					
+
+
+//--------------------------------
