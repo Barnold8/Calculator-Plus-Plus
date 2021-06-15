209321c8c71c59dcf249a3a441ad8187cd05acd1
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2021-06-15 23:52:46 +0100

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Calc.h"
+
+
+
+
+int main()
+{
+    //Currently empty so I can work on the header files and their forward declaration to provide functionality to the program
+
+   
+    
+  
+}
